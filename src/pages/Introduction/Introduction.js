2631ca8f8b2ff739b2301ee3ec6a97af0a571688
Vinay Jatav <vinay.jatav.dev@gmail.com>
2023-12-20 13:07:39 +0530

    Introduction Completed

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -2,20 +2,28 @@ import "./Introduction.css";
 
 export function Introduction() {
     return (
-        <div className="introduction-container">
-            <div>
+        <div>
+            <div className="introduction-container">
                 <div>
-                    <p className="name-text">Vinay</p>
-                    <p className="name-text margin-set">Jatav <span>.</span></p>
+                    <div>
+                        <p className="name-text">Vinay</p>
+                        <p className="name-text margin-set">Jatav <span>.</span></p>
+                    </div>
+                    <div className="bar"></div>
+                    <p className="introduction-sm-text">Instagram | LinkedIn</p>
+                    <p className="introduction-sm-text">Dribble | Behance</p>
                 </div>
-                <div className="bar"></div>
-                <p className="introduction-sm-text">Instagram | LinkedIn</p>
-                <p className="introduction-sm-text">Dribble | Behance</p>
+                <div className="image-container introduction-container-child">
+                    <img src="./images/main.png"></img>
+                </div>
+                <div className="introduction-text hide-from-mobile">
+                    <p className="introduction-header-highlight">INTRODUCTION</p>
+                    <p className="lg-text">Full Stack Web</p>
+                    <p className="lg-text">Developer</p>
+                    <p className="introduction-sm-text">Highly motivated and detail-oriented web developer with a strong foundation in the MERN (MongoDB, Express.js, React.js, Node.js) stack.</p>
+                </div>
             </div>
-            <div className="image-container introduction-container-child">
-                <img src="./images/main.png"></img>
-            </div>
-            <div className="introduction-text hide-from-mobile">
+            <div className="introduction-mobile only-mobile">
                 <p className="introduction-header-highlight">INTRODUCTION</p>
                 <p className="lg-text">Full Stack Web</p>
                 <p className="lg-text">Developer</p>
