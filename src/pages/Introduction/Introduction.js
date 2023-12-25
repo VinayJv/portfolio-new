649bc7769f3ff9def38141d739784f1a9673c4de
Vinay Jatav <vinay.jatav.dev@gmail.com>
2023-12-25 21:15:50 +0530

    Update Introduction.js

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -17,17 +17,17 @@ export function Introduction() {
                 <div className="image-container">
                     <img src="./images/main.png" alt=""></img>
                 </div>
-                <div className="introduction-text hide-from-mobile">
+                {/* <div className="introduction-text hide-from-mobile">
                     <p className="introduction-header-highlight">INTRODUCTION</p>
                     <p className="lg-text">Full Stack Web</p>
                     <p className="lg-text">Developer</p>
                     <p className="introduction-sm-text">Highly motivated and detail-oriented web developer with a strong foundation in the MERN (MongoDB, Express.js, React.js, Node.js) stack.</p>
-                </div>
+                </div> */}
             </div>
-            <div className="introduction-mobile only-mobile">
+            <div className="introduction-mobile">
                     <p className="introduction-header-highlight introduction-header-highlight-mobile">INTRODUCTION</p>
                     <p className="lg-text">Full Stack Web <span>Developer</span></p>
-                    <p className="introduction-sm-text">Highly motivated and detail-oriented web developer with a strong foundation in the MERN (MongoDB, Express.js, React.js, Node.js) stack.</p>
+                    <p className="introduction-sm-text">Highly motivated and detail-oriented web developer with a strong foundation in the MERN (MongoDB, Express.js, React.js, Node.js) stack. A rapid learner dedicated to efficiently assimilating new concepts, and apply newfound knowledge to elevate the quality of web development projects. </p>
             </div>
         </>);
 }
